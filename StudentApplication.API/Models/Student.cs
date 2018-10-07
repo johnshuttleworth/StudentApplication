d05024c5d12658227b2a2849574d04fa54a3ca4a
johnshuttleworth <johnshuttleworth@hotmail.com>
2018-10-07 20:21:42 +0100

    Add project files.

---
+++ StudentApplication.API/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentApplication.API.Models
+{
+    public class Student
+    {
+    }
+}
